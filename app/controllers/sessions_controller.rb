a62a66c185d95792a2ffb4aaa0e4531ba132d072
Carly <cfujiyoshi@gmail.com>
2017-07-05 19:12:21 -0700

    Can authenticate users off JSON

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,25 @@
 class SessionsController < ApplicationController
+
+  # has_secure_password
+
   def new
+    students = Unirest.get("http://localhost:3001/api/v1/students/").body
+    @student = students.first
+    @id = students.first["id"]
     render 'new.html.erb'
   end
 
   def create
-    
+    students = Unirest.get("http://localhost:3001/api/v1/students/").body
+    student = students.first
+    id = student["id"]
+    encrypted_password = BCrypt::Password.create(params[:password])
+    if student && student["password"] === params[:password]
+      session[:student_id] = student["id"]
+      redirect_to "/students/#{id}"
+    else
+      redirect_to "/login"
+    end
   end
+
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
+  
+  # has_secure_password
+
   self.abstract_class = true
 end
